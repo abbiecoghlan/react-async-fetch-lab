5c179240c3fec66680c06fe495da03ca2bf518ec
“Abbie <abbie.coghlan@gmail.com>
2021-03-04 17:09:10 -0600

    commit tree 49136d6aaed83f22cec91362f61b42d4103cf24a

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React from 'react';
+
+class App extends React.Component {
+
+    state = {
+        spaceData: []
+    }
+componentDidMount(){
+    fetch('http://api.open-notify.org/astros.json')
+    .then(res => res.json())
+    .then((data) => this.setState({ spaceData: data }))
+}
+
+render() {
+    return (
+      <div>
+          </div>
+    )
+  }
+
+}
+
+export default App
